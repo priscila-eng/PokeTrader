d806a00f27e200b90117f85f7346f438196bfccf
priscila-eng <priscilaangell123@gmail.com>
2021-06-02 08:18:19 -0300

    Remove logica de login

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,16 @@ import React from "react";
 import { Route, Switch, Router } from "react-router";
 
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import TradeHistory from "./pages/TradeHistory";
 import ViewResults from "./pages/ViewResults";
 import { history } from "./utils/history";
 function Routes() {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact={true} path="/" component={Login} />
         <Route path="/results" component={ViewResults} />
-        <Route path="/calculator" component={Home} />
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/history" component={TradeHistory} />
       </Switch>
     </Router>
   );
--- src/services/apiUser.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const apiUser = axios.create({
-  baseURL: "http://localhost:3001",
-});
-
-export default apiUser;
